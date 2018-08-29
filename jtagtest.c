dbc5fd36ee80184147a58040aaab0ebedb814a3f
Aksel Danielsen <akselsd@stud.ntnu.no>
2018-08-29 14:56:46 +0200

    created jtagtest.c

---
+++ jtagtest.c
@@ -0,0 +1,18 @@
+#define F_CPU 4915200
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define SET_BIT(reg, bit) (reg |= (1 << bit))
+#define CLEAR_BIT(reg, bit) (reg &= ~(1 << bit))
+
+int main()
+{
+	SET_BIT(DDRC, PIN0);
+
+	while(1){
+		SET_BIT(PORTC, PIN0);
+		_delay_ms(20);
+		CLEAR_BIT(PORTC, PIN0);
+		_delay_ms(20);
+	}
+}
