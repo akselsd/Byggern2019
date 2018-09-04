e4d05ddb503c8d7dc536e3c8f1871aecdb69a6b1
Aksel Danielsen <akselsd@stud.ntnu.no>
2018-09-04 12:01:47 +0200

    Interuppt driven uart

--- uart.c
+++ uart.c
@@ -1,10 +1,24 @@
 #include <avr/io.h> //Trengs denne?
 #include <stdio.h>
+#include <stdlib.h> // Abort
+#include <avr/interrupt.h>
 #include "uart.h"
+#define BUFFER_SIZE 256
 
 
 #define SET_BIT(reg, bit) (reg |= (1 << bit))
 #define CLEAR_BIT(reg, bit) (reg &= ~(1 << bit))
+
+struct ringbuffer
+{
+	char buffer[BUFFER_SIZE];
+	int next_out;
+	int next_in;
+	int size;
+};
+
+struct ringbuffer send_buffer = {{0}, 0, 0, 0};
+struct ringbuffer recieve_buffer = {{0}, 0, 0, 0};
 
 
 void uart_init(const unsigned int cpu_frq, const unsigned int baudrate)
@@ -15,25 +29,71 @@ void uart_init(const unsigned int cpu_frq, const unsigned int baudrate)
 
 	// Burde disse bytte plass?
 	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
-	UCSR0C = (1<<URSEL0)|(1<<USBS0)|(3<<UCSZ00);
-
+	UCSR0C = (1<<URSEL0)|(1<<USBS0)|(3<<UCSZ00); //Endre til UCSZ01 og UCSZ00?
+	UCSR0B |= (1 << RXCIE0); //Recieve interrupt
+	sei(); // Enable interrupts
 	fdevopen(uart_send_char, uart_recieve_char);	
 }
 
+// Sjekke errorflag?
 int uart_send_char(char c, FILE* dummy)
 {
-	while (!(UCSR0A & (1 << UDRE0)));
-	UDR0 = c;
+	cli();
+	if (send_buffer.size >= BUFFER_SIZE)
+		abort(); // Handle overflow here. Return 1 to fdevopen?
 
-	return 0;
+	if (send_buffer.size == 0)
+		UCSR0B |= (1 << UDRIE0); //Enable Transmit register empty interrupt
+
+	send_buffer.buffer[send_buffer.next_in++] = c;
+	++send_buffer.size;
+	
+
+	if (send_buffer.next_in >= BUFFER_SIZE)
+		send_buffer.next_in = 0;
+
+	sei();
+	return 0; // Success
 }
 
-
+// Sjekke errorflag?
 int uart_recieve_char(FILE* dummy)
 {
-	while(!(UCSR0A & (1 << RXC0)))
+	cli();
+	// Block if there is no current input
+	while (recieve_buffer.size == 0);
 
-	return UDR0;
+	char c = recieve_buffer.buffer[recieve_buffer.next_out++];
+	if (recieve_buffer.next_out == BUFFER_SIZE)
+		recieve_buffer.next_out = 0;
+
+	--recieve_buffer.size;
+	sei();
+	return c;
+}
+
+ISR(USART0_RXC_vect)
+{
+	if (recieve_buffer.size == BUFFER_SIZE)
+		abort(); // Handle overflow here;
+
+	recieve_buffer.buffer[recieve_buffer.next_in++] = UDR0;
+	if (recieve_buffer.next_in == BUFFER_SIZE)
+		recieve_buffer.next_in = 0;
+
+	++recieve_buffer.size;
+}
+
+ISR(USART0_UDRE_vect)
+{
+	UDR0 = send_buffer.buffer[send_buffer.next_out++];
+	if (send_buffer.next_out == BUFFER_SIZE)
+		send_buffer.next_out = 0;
+	
+	--send_buffer.size;
+	if (send_buffer.size == 0)
+		UCSR0B &= ~(1 << UDRIE0); //Disable Transmit register empty interrupt
+
 }
 //TXEN Transmit Enable
 //UMSEL bit in UCSRC velger Async/Sync (0 for Async)
