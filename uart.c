54cc9a8649e2753ace3c1d4ceb979569ce0963d1
Bernhard Paus Græsdal <bernhardpg@dhcp-10-24-35-34.wlan.ntnu.no>
2018-09-05 09:46:07 +0200

    added explaining comments to uart.c

--- uart.c
+++ uart.c
@@ -10,11 +10,15 @@
 void uart_init(const unsigned int cpu_frq, const unsigned int baudrate)
 {
 	int ubrr = cpu_frq/16/baudrate - 1;
+
+	/* Set baud rate */
 	UBRR0H = (unsigned char)(ubrr >> 8); // High bits of counter
 	UBRR0L = (unsigned char)ubrr; // Low bits of counter
 
 	// Burde disse bytte plass?
+	/* Enable receiver and transmitter */
 	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+	/* Set frame format: 8data, 2stop bit */
 	UCSR0C = (1<<URSEL0)|(1<<USBS0)|(3<<UCSZ00);
 
 	fdevopen(uart_send_char, uart_recieve_char);	
@@ -22,7 +26,10 @@ void uart_init(const unsigned int cpu_frq, const unsigned int baudrate)
 
 int uart_send_char(char c, FILE* dummy)
 {
+	/* Wait for empty transmit buffer */
 	while (!(UCSR0A & (1 << UDRE0)));
+
+	/* Put data into buffer, sends the data */
 	UDR0 = c;
 
 	return 0;
@@ -31,10 +38,14 @@ int uart_send_char(char c, FILE* dummy)
 
 int uart_recieve_char(FILE* dummy)
 {
+	/* Wait for data to be received */
 	while(!(UCSR0A & (1 << RXC0)))
 
+	/* Get and return received data from buffer */
 	return UDR0;
 }
+
+
 //TXEN Transmit Enable
 //UMSEL bit in UCSRC velger Async/Sync (0 for Async)
 //UBRR
