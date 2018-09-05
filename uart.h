0f4a4d7df55d39523a406b403daca34d261e2eba
Aksel Danielsen <akselsd@stud.ntnu.no>
2018-09-05 11:53:15 +0200

    Uart.h cleanup

--- uart.h
+++ uart.h
@@ -1,12 +1,11 @@
 #ifndef INCLUDE_UART_H
 #define INCLUDE_UART_H
 #include <stdio.h>
-//Make interrupt?
 
 void uart_init(const unsigned int ubrr);
-int uart_send_char(char c, FILE* neverused); // Gjøre static?
-int uart_recieve_char(FILE* neverused); // Gjøre static?
 int uart_flush_send_buffer(void);
 
-
+/* Functions not used outside uart.c Make static and hide from interface? */
+int uart_send_char(char c, FILE* neverused);
+int uart_recieve_char(FILE* neverused);
 #endif
