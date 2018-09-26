9f682fe8fd08f1aeeef0c0fff00c63040597e430
Aksel Danielsen <akselsd@stud.ntnu.no>
2018-09-26 12:43:00 +0200

    Print formatting

--- tests/usb_multifunction_board_test.c
+++ tests/usb_multifunction_board_test.c
@@ -9,13 +9,13 @@ void test_usb_multifunction_board(void)
     buttons_status bstatus = usb_multifunction_buttons_get_status();
    	slider_status sstatus = slider_get_status();
     joystick_status jstatus = joystick_get_status();
-    printf("X: %d, Y: %d, Dir: %s, JB: %d, BR: %d, BL: %d, R: %u, L: %u\n",
+    printf("X: %4d, Y: %4d, Dir: %12s, JB: %d, BL: %d, BR: %d, L: %3u, R: %4u\n",
     	jstatus.x,
     	jstatus.y,
     	joystick_direction_to_string(jstatus.dir),
     	jstatus.pressed,
-    	bstatus.right,
     	bstatus.left,
-    	sstatus.right,
-    	sstatus.left);
+    	bstatus.right,
+    	sstatus.left,
+    	sstatus.right);
 }
