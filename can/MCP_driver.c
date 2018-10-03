cbb613445c52c0fdf323157037497785a5cf365a
Bernhard Paus <bernhardpg@gmail.com>
2018-10-03 15:02:58 +0200

    Fixed MCP drivers

--- can/MCP_driver.c
+++ can/MCP_driver.c
@@ -2,103 +2,111 @@
 #include "SPI_driver.h"
 #include "MCP_driver.h"
 #include "MCP2515.h"
-#include <avr/delay.h>
+#include <util/delay.h>
 #include <stdio.h>
-
-
-void MCP_write(uint8_t address, uint8_t data)
-{
-	SPI_start_transmit();
-
-	//_delay_ms(10); TODO is this necessary?
-	SPI_transmit(MCP_WRITE);
-	SPI_transmit(address);
-	SPI_transmit(data);
-
-	SPI_end_transmit();
-}
-
-
-uint8_t MCP_read(uint8_t address)
-{
-	SPI_start_transmit();
-
-	uint8_t data;
-	SPI_transmit(MCP_READ);
-	data = SPI_transmit(address);
-
-	SPI_end_transmit();
-	return data;
-}
-
-void MCP_request_to_sent(uint8_t buffer)
-{
-	SPI_start_transmit();
-
-	switch(buffer)
-	{
-		case BUFFER_T0:
-			SPI_transmit(MCP_RTS_TX0);
-			break;
-		case BUFFER_T1:
-			SPI_transmit(MCP_RTS_TX1);
-			break;
-		case BUFFER_T2:
-			SPI_transmit(MCP_RTS_TX2);
-			break;
-		case BUFFER_ALL:
-			SPI_transmit(MCP_RTS_ALL);
-			break;
-				
-	}
-
-	SPI_end_transmit();
-}
-
-uint8_t MCP_read_status(void)
-{
-	SPI_start_transmit();
-
-	uint8_t status;
-	status = SPI_transmit(MCP_READ_STATUS);
-
-	SPI_end_transmit();
-
-	return status;
-}
-
-void MCP_bit_modify(uint8_t address, uint8_t mask, uint8_t data)
-{
-	SPI_start_transmit();
-
-	SPI_transmit(MCP_BITMOD);
-	SPI_transmit(address);
-	SPI_transmit(mask);
-	SPI_transmit(data);
-
-	SPI_end_transmit();
-}
 
 void MCP_init(void)
 {
-	printf("MCP_CAN_STATUS: %d\n", MCP_read(MCP_CANSTAT));
-	printf("Resetting MCP. \n");
+	SPI_master_init();
 	MCP_reset();
-	printf("MCP_CAN_STATUS: %d\n", MCP_read(MCP_CANSTAT));
+	//printf("Status: %d\n", MCP_read_status());
 
-	/* Enter normal mode */
-	MCP_write(MCP_CANCTRL, MODE_NORMAL);
 
-	printf("MCP_CAN_STATUS: %d\n", MCP_read(MCP_CANSTAT));
-	_delay_ms(1000);
-	printf("MCP_CAN_STATUS: %d\n", MCP_read(MCP_CANSTAT));
+	/* Wait for reset */
+	_delay_ms(100);
+
+	/* TODO LOOPBACK for debug */
+	MCP_write(MCP_CANCTRL, MODE_LOOPBACK);
+	//printf("Status: %d\n", MCP_read_status());
+
 }
 
 void MCP_reset(void)
 {
 	SPI_start_transmit();
 	//_delay_ms(10); TODO is this necessary?
-	SPI_transmit(MCP_RESET);
+	SPI_write(MCP_RESET);
 	SPI_end_transmit();
 }
+
+uint8_t MCP_read(uint8_t address)
+{
+	SPI_start_transmit();
+
+	uint8_t data;
+	SPI_write(MCP_READ);
+	SPI_write(address);
+
+	data = SPI_read();
+
+	SPI_end_transmit();
+	return data;
+}
+
+void MCP_read_n(uint8_t address, uint8_t * data, uint8_t n_bytes)
+{
+	SPI_start_transmit();
+
+	SPI_write(MCP_READ);
+	SPI_write(address);
+	for (int i = 0; i < n_bytes; ++i)
+	{
+		data[i] = SPI_read();
+	}
+
+	SPI_end_transmit();
+}
+
+void MCP_write(uint8_t address, uint8_t data)
+{
+	SPI_start_transmit();
+
+	//_delay_ms(10); TODO is this necessary?
+	SPI_write(MCP_WRITE);
+	SPI_write(address);
+	SPI_write(data);
+	SPI_end_transmit();
+}
+
+void MCP_write_n(uint8_t address, uint8_t * data, uint8_t n_bytes)
+{
+	SPI_start_transmit();
+	SPI_write(MCP_WRITE);
+	SPI_write(address);
+	for (int i = 0; i < n_bytes; ++i)
+	{
+		SPI_write(data[i]);
+	}
+
+	SPI_end_transmit();
+}
+
+void MCP_request_to_send(void)
+{
+	SPI_start_transmit();
+
+	SPI_write(MCP_RTS_TX0);
+
+	SPI_end_transmit();
+}
+
+uint8_t MCP_read_status(void)
+{
+	/* Returns MCP_CANSTAT reg. */
+	return MCP_read(MCP_READ_STATUS);
+}
+
+void MCP_bit_modify(uint8_t address, uint8_t mask, uint8_t data)
+{
+	SPI_start_transmit();
+
+	SPI_write(MCP_BITMOD);
+	SPI_write(address);
+	SPI_write(mask);
+	SPI_write(data);
+
+	SPI_end_transmit();
+}
+
+
 
--- can/MCP_driver.h
+++ can/MCP_driver.h
@@ -2,11 +2,13 @@
 #define MCP_DRIVERS_H_INCLUDED
 #include <stdint.h>
 
-enum buffer {BUFFER_T0, BUFFER_T1, BUFFER_T2, BUFFER_ALL};
-
+void MCP_init(void);
+void MCP_reset(void);
 uint8_t MCP_read(uint8_t address);
+void MCP_read_n(uint8_t address, uint8_t * data, uint8_t n_bytes);
 void MCP_write(uint8_t address, uint8_t data);
-void MCP_request_to_sent(uint8_t buffer);
+void MCP_write_n(uint8_t address, uint8_t * data, uint8_t n_bytes);
+void MCP_request_to_send(void);
 uint8_t MCP_read_status(void);
 void MCP_bit_modify(uint8_t address, uint8_t mask, uint8_t data);
 
