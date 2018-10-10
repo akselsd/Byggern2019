24d724f42d7c40c05fb63cd040b627f050745b54
Bernhard Paus <bernhardpg@gmail.com>
2018-10-10 15:38:54 +0200

    Added node2 initial files

---
+++ node2/constants/system_constants.h
@@ -0,0 +1,12 @@
+#ifndef SYSTEM_CONTANTS_H_INCLUDED
+#define SYSTEM_CONTANTS_H_INCLUDED
+
+#define F_CPU 16000000 // 16 MHz
+#define RX_VECTOR USART0_RX_vect
+#define UART_DATA_BIT USBS0
+#define MOSI_PIN DDB2
+#define SCK_PIN DDB1
+#define SS_PIN PB0
+#define SELECT_PIN PB7
+
+#endif
--- node2/main.c
+++ node2/main.c
@@ -1,10 +1,13 @@
-#define F_CPU 4915200
+#include "system_constants.h"
 #define BAUDRATE 9600
 #define UBRR F_CPU/16/BAUDRATE - 1
+#include "uart/uart.h"
+#include "can/CAN_driver.h"
 #include <stdio.h> //Trengs denne?
 #include <avr/io.h>
 #include <stdio.h>
 #include <util/delay.h>
+#include "../node1/usb_multifunction_card/joystick.h"
 //#include "utils/uart.h"
 
 #define SET_BIT(reg, bit) (reg |= (1 << bit))
@@ -14,7 +17,9 @@ int MY_PINS[8] = {PIN0, PIN1, PIN2, PIN3, PIN4, PIN5, PIN6, PIN7};
 
 void init_all(void)
 {
-	//uart_init(UBRR);
+	uart_init(UBRR);
+	printf("Uart init\n");
+	CAN_init();
 }
 
 int main()
@@ -22,10 +27,21 @@ int main()
 	/* Initialize system */
 	init_all();
 	_delay_ms(1000);
-	printf("\n\n\nInitialized\n");
+	printf("\n\n\nNode 2 Initialized\n\n\n");
 
 	while(1){
-		printf("UART WORKING.\n");
+		CAN_message received_msg;
+		CAN_receive(&received_msg);
+
+		joystick_status status = {
+			received_msg.data[0],
+			received_msg.data[1],
+			received_msg.data[2],
+			received_msg.data[3],
+		};
+
+		printf("Joystick_status: x: %3d, y: %3d, pressed: %d, dir: %1d \n",
+			status.x, status.y, status.pressed, status.dir);
 	};
     
 }
