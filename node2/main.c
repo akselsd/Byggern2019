0becaf6d4a42f00ed5fd0163b45a0f2decd93711
Aksel Danielsen <akselsd@stud.ntnu.no>
2018-10-24 17:16:06 +0200

    Cleaned up main.c for node1 and node2

--- node2/main.c
+++ node2/main.c
@@ -6,14 +6,13 @@
 #include <avr/io.h>
 #include <stdio.h>
 #include <util/delay.h>
-#include "uart/uart.h"
+
+#include "adc_driver.h"
+#include "bit_macros.h"
 #include "can/CAN_driver.h"
 #include "game_board_driver.h"
-
-#define SET_BIT(reg, bit) (reg |= (1 << bit))
-#define CLEAR_BIT(reg, bit) (reg &= ~(1 << bit))
-
-int MY_PINS[8] = {PIN0, PIN1, PIN2, PIN3, PIN4, PIN5, PIN6, PIN7};
+#include "pwn_driver.h"
+#include "uart/uart.h"
 
 void init_all(void)
 {
@@ -21,6 +20,7 @@ void init_all(void)
 	printf("Uart init\n");
 	CAN_init();
 	pwm_init();
+	adc_init();
 }
 
 int main()
@@ -31,11 +31,13 @@ int main()
 	printf("\n\n\nNode 2 Initialized\n\n\n");
 
 	while(1){
+		
 		CAN_message * msg = CAN_receive();
 		printf("x: %d\n", msg->data[1]);
 		game_board_receive(msg);
 		CAN_message_destructor(msg);
 		_delay_ms(50);
+
 
 		//printf("Joystick_status: x: %3d, y: %3d, pressed: %d, dir: %1d \n",
 			//status.x, status.y, status.pressed, status.dir);
